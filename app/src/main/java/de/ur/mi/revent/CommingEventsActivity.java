aefec5b62664e769ec8741128d1574fb913f4084
Peters Christian <Christian.Peters@stud.uni-regensburg.de>
2018-09-07 05:38:11 +0200

    Revert "Delete CommingEvents"
    This reverts commit b6f742a44500a3da745070497f0a37d2c0aaa4f1.

---
+++ app/src/main/java/de/ur/mi/revent/CommingEventsActivity.java
@@ -0,0 +1,58 @@
+package de.ur.mi.revent;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+import de.ur.mi.revent.Download.DownloadListener;
+import de.ur.mi.revent.Download.DataDownload;
+import de.ur.mi.revent.Download.DownloadManager;
+import de.ur.mi.revent.Template.EventItem;
+
+import de.ur.mi.revent.Download.DownloadListener;
+
+public class CommingEventsActivity extends Activity implements DownloadListener {
+    private ArrayList<EventItem> table;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_events);
+        getDownloadData();
+    }
+
+    @Override
+    public void onDownloadFinished() {
+        try {
+            table = DownloadManager.getResults();
+            printData();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getDownloadData(){
+        try {
+            if (DownloadManager.getStatus() == AsyncTask.Status.FINISHED) {
+                table = DownloadManager.getResults();
+                printData();
+            } else {
+                DownloadManager.setListener(this);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printData(){
+        System.out.println(table.get(2).getOrganizer());
+        System.out.println(table.size());
+        System.out.println("here");
+    }
+}
