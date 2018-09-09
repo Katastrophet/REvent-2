2ea43f01907bbda0d17dc5e3ae065d1a9936fbde
Peters Christian <Christian.Peters@stud.uni-regensburg.de>
2018-09-09 14:04:37 +0200

    Revert "Revert "Merge branch 'dev-peters'""
    This reverts commit 695d18be3a64db64d61c676637501a9db00ac182.

--- app/src/main/java/de/ur/mi/revent/VorgeschlageneEventsActivity.java
+++ app/src/main/java/de/ur/mi/revent/VorgeschlageneEventsActivity.java
@@ -1,16 +1,17 @@
 package de.ur.mi.revent;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
-import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -19,34 +20,40 @@ import de.ur.mi.revent.Download.DownloadListener;
 import de.ur.mi.revent.Download.DownloadManager;
 import de.ur.mi.revent.Template.EventItem;
 
-public class KommendeEvents extends Activity implements DownloadListener{
+public class VorgeschlageneEventsActivity extends Activity implements DownloadListener {
     private _NavigationMenu navigationMenu;
     //EventList
+    private ArrayList<EventItem> tableVorgeschlageneEvents;
     private ArrayList<EventItem> table;
-    private ListView eventList_KE;
+    private ListView eventList_VorgeschlageneEvents;
     private _EventItemArrayAdapter aa;
     //
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ke);
-        navigationMenu=new _NavigationMenu(this);
+        setContentView(R.layout.activity_vorgeschlagene_events);
+        navigationMenu = new _NavigationMenu(this);
         //EventList
-        eventList_KE=(ListView)findViewById(R.id.eventList_KE);
+        tableVorgeschlageneEvents=new ArrayList<>();
+        eventList_VorgeschlageneEvents=(ListView)findViewById(R.id.eventList_vorgeschlageneEvents);
         table = new ArrayList<EventItem>();
         getDownloadData();
-        Collections.sort(table);
-        aa=new _EventItemArrayAdapter(this,R.layout.event_list_items,table);
-        eventList_KE.setAdapter(aa);
+        fillVorgeschlageneEvents();
+        aa=new _EventItemArrayAdapter(this,R.layout.event_list_items,tableVorgeschlageneEvents);
+        eventList_VorgeschlageneEvents.setAdapter(aa);
         //aa.notifyDataSetChanged();
 
-        eventList_KE.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+        eventList_VorgeschlageneEvents.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                EventItem eventItem =(EventItem) eventList_KE.getItemAtPosition(i);
+                EventItem eventItem =(EventItem) eventList_VorgeschlageneEvents.getItemAtPosition(i);
                 navigationMenu.showEvent(eventItem.getTitle(),eventItem.getDate().toString(),eventItem.getTime().toString(),eventItem.getLocation(),eventItem.getOrganizer(),eventItem.getType());
             }
         });
         //
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -58,18 +65,10 @@ public class KommendeEvents extends Activity implements DownloadListener{
         navigationMenu.onOptionsItemSelected(item);
         return true;
     }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        Intent i = new Intent(this, MainActivity.class);
-        startActivity(i);
-    }
     @Override
     public void onDownloadFinished() {
         try {
             table = DownloadManager.getResults();
-            printData();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -78,7 +77,6 @@ public class KommendeEvents extends Activity implements DownloadListener{
         try {
             if (DownloadManager.getStatus() == AsyncTask.Status.FINISHED) {
                 table = DownloadManager.getResults();
-                printData();
             } else {
                 DownloadManager.setListener(this);
             }
@@ -86,9 +84,23 @@ public class KommendeEvents extends Activity implements DownloadListener{
             e.printStackTrace();
         }
     }
-    private void printData(){
-        System.out.println(table.get(2).getOrganizer());
+    private void fillVorgeschlageneEvents(){
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        for (int i=0;table.size()>i;i++){
+            switch (table.get(i).getType()){
+                case "Party": if (sharedPref.getBoolean("pref_party",true)){
+                    tableVorgeschlageneEvents.add(table.get(i));
+                    }break;
+                case "Ausflug":
+                    if (sharedPref.getBoolean("pref_ausflug",true)){
+                        tableVorgeschlageneEvents.add(table.get(i));
+                    }break;
+                case "Vortrag":
+                    if (sharedPref.getBoolean("pref_vortrag",true)){
+                        tableVorgeschlageneEvents.add(table.get(i));
+                    }break;
+            }
+        }
+        Collections.sort(tableVorgeschlageneEvents);
     }
-
-
 }
